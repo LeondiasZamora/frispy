8b63789cf0e85b462e9e13dd617e844dc19e0799
Babadi MUKANYA <babadi.mukanya@gmail.com>
2024-11-26 17:27:55 +0100

    modified:   app/assets/stylesheets/pages/_home.scss 	modified:   app/controllers/food_items_controller.rb 	modified:   app/models/food_item.rb 	modified:   app/models/user.rb 	modified:   app/views/food_items/index.html.erb 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/pages/home.html.erb 	modified:   config/routes.rb 	modified:   db/schema.rb

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -1,30 +1,49 @@
 class FoodItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_food_item, only: [:edit, :update, :destroy]
 
   def index
-
-  end
-
-  def show
-
+    @food_items = current_user.food_items.order(expiry_date: :asc, created_at: :asc)
   end
 
   def new
-
+    @food_item = current_user.food_items.new
   end
 
   def create
-
+    @food_item = current_user.food_items.new(food_item_params)
+    if @food_item.save
+      redirect_to food_items_path, notice: "Food item added successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
-
   end
 
   def update
-
+    if @food_item.update(food_item_params)
+      redirect_to food_items_path, notice: "Food item updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @food_item.destroy
+    redirect_to food_items_path, notice: "Food item deleted successfully."
+  end
 
+  private
+
+  def set_food_item
+    @food_item = current_user.food_items.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to food_items_path, alert: "Food item not found."
+  end
+
+  def food_item_params
+    params.require(:food_item).permit(:name, :quantity, :unit, :nutriscore, :shelf_life, :expiry_date)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_25_155248) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_26_135940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_155248) do
     t.string "nutri_score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_food_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_155248) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "food_items", "users"
 end
