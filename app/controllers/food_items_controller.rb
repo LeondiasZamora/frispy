9ae08c1014609aad4af3d9f091047136636c95da
Salman Tsagaraev <salman.tsagaraev@gmail.com>
2024-11-26 12:02:45 +0100

    created seeds and updated the controller page

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -1,30 +1,36 @@
 class FoodItemsController < ApplicationController
 
   def index
-
+    @food_items = FoodItem.all
   end
 
   def show
-
+    @food_item = FoodItem.find(params[:id])
   end
 
   def new
-
+    @food_item = FoodItem.new
   end
 
   def create
-
+    @food_item = FoodItem.new(food_item_params)
   end
 
   def edit
-
+    @food_item = FoodItem.find(params[:id])
   end
 
   def update
-
+    @food_item = FoodItem.find(params[:id])
   end
 
   def destroy
+    @food_item = FoodItem.find(params[:id])
+  end
 
+  private
+
+  def food_item_params
+    params.require(:food_item).permit(:name, :quantity, :expiry_date, :expected_lifetime, :nutry_score, images: [])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,68 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+
+# db/seeds.rb
+
+# Clear existing data to avoid duplication
+FoodItem.destroy_all
+
+# Seed data for FoodItem
+food_items = [
+  {
+    name: "Apple",
+    quantity: 20,
+    expiry_date: Date.today + 10,
+    expected_lifetime: 14, # days
+    nutri_score: "A"
+  },
+  {
+    name: "Bread",
+    quantity: 10,
+    expiry_date: Date.today + 5,
+    expected_lifetime: 7, # days
+    nutri_score: "B"
+  },
+  {
+    name: "Carrots",
+    quantity: 15,
+    expiry_date: Date.today + 12,
+    expected_lifetime: 21, # days
+    nutri_score: "A"
+  },
+  {
+    name: "Milk",
+    quantity: 8,
+    expiry_date: Date.today + 7,
+    expected_lifetime: 10, # days
+    nutri_score: "C"
+  },
+  {
+    name: "Eggs",
+    quantity: 30,
+    expiry_date: Date.today + 14,
+    expected_lifetime: 21, # days
+    nutri_score: "B"
+  },
+  {
+    name: "Chicken Breast",
+    quantity: 5,
+    expiry_date: Date.today + 3,
+    expected_lifetime: 4, # days
+    nutri_score: "C"
+  },
+  {
+    name: "Rice",
+    quantity: 50,
+    expiry_date: Date.today + 365,
+    expected_lifetime: 730, # days
+    nutri_score: "A"
+  }
+]
+
+# Create records in the database
+food_items.each do |item|
+  FoodItem.create!(item)
+end
