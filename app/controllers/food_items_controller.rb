003218ab3ea3cebc66c92a9fc49726ab013312e8
Leonidas Zamora Gottwald <leonidas.zamora.gottwald@gmail.com>
2024-11-26 15:53:32 +0100

    finished the search bar and the inventory

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -1,11 +1,17 @@
 class FoodItemsController < ApplicationController
+  before_action :setup_food_item, only: %i[show edit update destroy]
 
   def index
-    @food_items = FoodItem.all
+    if params[:query].present?
+      food_list = FoodItem.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      food_list = FoodItem.all
+    end
+    @food_items = food_list.sort_by(&:expiry_date)
   end
 
   def show
-    @food_item = FoodItem.find(params[:id])
+
   end
 
   def new
@@ -17,20 +23,24 @@ class FoodItemsController < ApplicationController
   end
 
   def edit
-    @food_item = FoodItem.find(params[:id])
+
   end
 
   def update
-    @food_item = FoodItem.find(params[:id])
+
   end
 
   def destroy
-    @food_item = FoodItem.find(params[:id])
+
   end
 
   private
 
+  def setup_food_item
+    @food_item = FoodItem.find(params[:id])
+  end
+
   def food_item_params
-    params.require(:food_item).permit(:name, :quantity, :expiry_date, :expected_lifetime, :nutry_score, images: [])
+    params.require(:food_item).permit(:name, :quantity, :expiry_date, :expected_lifetime, :nutri_score, images: [])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,51 +19,51 @@ FoodItem.destroy_all
 food_items = [
   {
     name: "Apple",
-    quantity: 20,
-    expiry_date: Date.today + 10,
-    expected_lifetime: 14, # days
+    quantity: 5,
+    expiry_date: Date.today + 4,
+    expected_lifetime: 4, # days
     nutri_score: "A"
   },
   {
     name: "Bread",
-    quantity: 10,
-    expiry_date: Date.today + 5,
-    expected_lifetime: 7, # days
+    quantity: 1,
+    expiry_date: Date.today + 13,
+    expected_lifetime: 13, # days
     nutri_score: "B"
   },
   {
     name: "Carrots",
-    quantity: 15,
-    expiry_date: Date.today + 12,
-    expected_lifetime: 21, # days
+    quantity: 6,
+    expiry_date: Date.today + 2,
+    expected_lifetime: 2, # days
     nutri_score: "A"
   },
   {
     name: "Milk",
-    quantity: 8,
-    expiry_date: Date.today + 7,
-    expected_lifetime: 10, # days
+    quantity: 2,
+    expiry_date: Date.today + 1,
+    expected_lifetime: 1, # days
     nutri_score: "C"
   },
   {
     name: "Eggs",
-    quantity: 30,
+    quantity: 18,
     expiry_date: Date.today + 14,
-    expected_lifetime: 21, # days
+    expected_lifetime: 4, # days
     nutri_score: "B"
   },
   {
     name: "Chicken Breast",
-    quantity: 5,
-    expiry_date: Date.today + 3,
-    expected_lifetime: 4, # days
+    quantity: 1,
+    expiry_date: Date.today + 10,
+    expected_lifetime: 10, # days
     nutri_score: "C"
   },
   {
     name: "Rice",
     quantity: 50,
-    expiry_date: Date.today + 365,
-    expected_lifetime: 730, # days
+    expiry_date: Date.today - 1,
+    expected_lifetime: 0, # days
     nutri_score: "A"
   }
 ]
