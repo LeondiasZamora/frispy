0437a64ee0a24c1fcde80698e016c55c19d75712
Leonidas Zamora Gottwald <leonidas.zamora.gottwald@gmail.com>
2024-12-02 20:49:48 +0100

    the instructions are now also displayed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,15 +4,14 @@ class RecipesController < ApplicationController
   end
 
   def create
-    instructions_array = []
-    params["instructions"].map do |instruction|
+    instructions_array = params["instructions"].map do |instruction|
       instruction["display_text"]
     end
+    joined_instructions = instructions_array.join
     nutrition = params[:nutrition]
-    pp nutrition[:calories]
     @recipe = Recipe.new(
       name: params[:name],
-      instructions: instructions_array.join,
+      instructions: joined_instructions,
       fat: nutrition["fat"],
       sugar: nutrition["sugar"],
       calories: nutrition["calories"],
