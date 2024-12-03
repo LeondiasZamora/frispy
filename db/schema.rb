8d9f9cb5dcfeebd59ffc6299da4694c21585478d
Salman Tsagaraev <salman.tsagaraev@gmail.com>
2024-12-03 15:53:04 +0100

    solved conflints

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.1].define(version: 2024_12_03_115833) do
-=======
-
-ActiveRecord::Schema[7.1].define(version: 2024_12_03_112232) do
-
->>>>>>> b458ad0eeeae3450e69e869e67f8c91e5c2d37e1
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
